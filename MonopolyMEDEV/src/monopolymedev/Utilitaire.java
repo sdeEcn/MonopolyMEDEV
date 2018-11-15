a52be56c9d586eaced06e880cf3f580390a75361
Elise <Elise@130.66.206.31>
2018-11-15 10:33:02 +0100

    retrait de position

--- MonopolyMEDEV/src/monopolymedev/Utilitaire.java
+++ MonopolyMEDEV/src/monopolymedev/Utilitaire.java
@@ -23,7 +23,6 @@ public class Utilitaire extends Achetable {
     /**
      * constructeur avec paramètre
      * @param nom nom de la case
-     * @param pos numéro de la caase sur le plateau
      * @param p prix de la case
      * @param pro propriétaire de la case
      * @param l loyer
