891cd5852191d8b220938e026c41a054802c65e3
Mael Guillossou <mael.guillossou@gmail.com>
2018-11-15 10:12:15 +0100

    Classe communauté

--- MonopolyMEDEV/src/monopolymedev/Communaute.java
+++ MonopolyMEDEV/src/monopolymedev/Communaute.java
@@ -5,10 +5,57 @@
  */
 package monopolymedev;
 
+import java.util.Random;
+
 /**
- *
+ * Classe représentant une carte Communaute
  * @author Mael Guillossou
  */
-public class Communaute {
-    
+public class Communaute extends Carte {
+
+    public Communaute() {
+    }
+
+    /**
+     * Pioche une carte communauté et applique l'effet au joueur
+     * @param joueur
+     */
+    public void piocher(Joueur joueur) {
+        Random random = new Random();
+        switch (random.nextInt(12)) {
+            case 0:
+                System.out.println("Aller au départ");
+                GoToPrison.envoyerPrison(joueur);
+                break;
+            case 1:
+                System.out.println("Aller au départ");
+                joueur.setPosition(0);
+                break;
+            case 2:
+                System.out.println("Aller Boulevard de la vilette");
+                joueur.setPosition(11);
+                break;
+            case 3:
+                System.out.println("Amende pour ivresse 2000");
+                joueur.setArgent(joueur.getArgent() - 2000);
+                break;
+            case 4:
+                System.out.println("Vous avez gagné mot-croisés 10000");
+                joueur.setArgent(joueur.getArgent() + 10000);
+                break;
+            case 5:
+                System.out.println("Allez rue Henri Martin");
+                joueur.setPosition(24);
+                break;
+            case 6:
+                System.out.println("Allez gare de lyon");case 7:
+                System.out.println("Amende pour excès de vitesse 1 500");
+                joueur.setArgent(joueur.getArgent() - 1500);
+                break;
+                joueur.setPosition(15);
+                break;
+
+
+        }
+    }
 }
