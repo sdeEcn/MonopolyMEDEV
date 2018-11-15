433f942551bd985dadfcb7ffed7656d4d95bcc8d
Elise <Elise@130.66.206.31>
2018-11-15 10:07:24 +0100

    classe utilitaire + javadoc

--- MonopolyMEDEV/src/monopolymedev/Utilitaire.java
+++ MonopolyMEDEV/src/monopolymedev/Utilitaire.java
@@ -6,20 +6,38 @@
 package monopolymedev;
 
 /**
- *
+ * classe utilitaire, sous classe de Achetable
+ * 
  * @author Elise
  */
 public class Utilitaire extends Achetable {
     //constructeurs
+
+    /**
+     * consstructeur par défaut
+     */
     public Utilitaire(){
         super();
     }
+
+    /**
+     * constructeur avec paramètre
+     * @param nom nom de la case
+     * @param pos numéro de la caase sur le plateau
+     * @param p prix de la case
+     * @param pro propriétaire de la case
+     * @param l loyer
+     */
     public Utilitaire(String nom, int pos, int p, Joueur pro, int l){
         super(nom,pos,p,pro,l);
     }
 
     
     //méthodes
+
+    /**
+     * 
+     */
     public void calculLoyer(){
         
     }  
