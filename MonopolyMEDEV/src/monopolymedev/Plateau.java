05041a743e785fae5585ba63b9dcf7be51a5144c
Thibault <thibault.cabourg@gmail.com>
2018-11-15 09:24:36 +0100

    Création de la classe plateau

---
+++ MonopolyMEDEV/src/monopolymedev/Plateau.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monopolymedev;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Thibault
+ */
+public class Plateau {
+    private ArrayList<Case> cases; 
+    private LinkedList<Joueur> joueurs; 
+    
+    public Plateau(){
+        this.cases=new ArrayList<>();
+        this.joueurs=new LinkedList<>();
+    }
+
+    public Plateau(ArrayList<Case> cases, LinkedList<Joueur> joueurs) {
+        this.cases = cases;
+        this.joueurs = joueurs;
+    }
+    
+    
+
+    public ArrayList<Case> getCases() {
+        return cases;
+    }
+
+    public LinkedList<Joueur> getJoueurs() {
+        return joueurs;
+    }
+
+    public void setCases(ArrayList<Case> cases) {
+        this.cases = cases;
+    }
+
+    public void setJoueurs(LinkedList<Joueur> joueurs) {
+        this.joueurs = joueurs;
+    }
+    
+    
+    
+}
